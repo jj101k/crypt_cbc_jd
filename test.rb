deaeb6575e358b5e05eea1e0ce0657c782e650f2
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-08-28 17:16:00 +0100

    Fix the AES test

--- test.rb
+++ test.rb
@@ -20,7 +20,7 @@ puts "Success!"
 puts "Now performing nice visual comparison (if this mismatches, there's a problem)"
 plaintext_hex = "4f682c20746865206772616e64206f6c642044756b65206f6620596f726b"
 
-%w[blowfish rijndael].each do |file|
+%w[blowfish rijndael aes].each do |file|
   require "jdcrypt/#{file}"
 rescue LoadError
   # Will be checked below
@@ -43,7 +43,7 @@ if defined? JdCrypt::AES
   tests_done += 1
   key_hex = "DB" * 16
   iv_hex = "00" * 16
-  fake_openssl = "./hex2bin.rb #{plaintext_hex} | #{ENV["_"]} ./openssl_like.rb rijndael AES #{key_hex} #{iv_hex} \
+  fake_openssl = "./hex2bin.rb #{plaintext_hex} | #{ENV["_"]} ./openssl_like.rb aes AES #{key_hex} #{iv_hex} \
     | ./bin2hex.rb"
   real_openssl = "./hex2bin.rb #{plaintext_hex} | openssl bf-cbc -nosalt -K #{key_hex} -iv #{iv_hex} | ./bin2hex.rb"
   puts fake_openssl
