e6801c37d6091670ff11c3ec12c91700e2acbc72
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-09-01 17:43:55 +0100

    Improve scale testing

--- openssl_like.rb
+++ openssl_like.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
-class_file, class_constant_name, key_hex, iv_hex = *ARGV
+class_file, class_constant_name, key_hex, iv_hex, in_q, in_v = *ARGV
 require "jdcrypt/#{class_file}"
 klass = JdCrypt.const_get(class_constant_name)
 key = [key_hex].pack("H*")
 iv = [iv_hex].pack("H*")
 require "./cbc"
-plaintext = $stdin.gets
+plaintext =
+  if in_q == "-in"
+    File.read(in_v)
+  else
+    $stdin.gets
+  end
 cipher = klass.new(key)
 cbc = JdCrypt::CBC.new(cipher)
 print cbc.encrypt(iv, plaintext)
--- test.rb
+++ test.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "tempfile"
 require "./crypt_dummy"
 plaintext = "12234567890dsgayufkgdadseluhkigfkukgkusdygfuewyetuygkuycvwegw,a3qwa"
 key = "!" * 16
@@ -17,13 +18,55 @@ raise unless chain_cipher.decrypt(iv, ciphertext) == plaintext
 
 puts "Success!"
 
-puts "Now performing nice visual comparison (if this mismatches, there's a problem)"
-plaintext_hex = "4f682c20746865206772616e64206f6c642044756b65206f6620596f726b"
-
 %w[blowfish rijndael aes].each do |file|
   require "jdcrypt/#{file}"
 rescue LoadError
   # Will be checked below
+end
+
+def run_bulk(command, plaintext)
+  # Rebuild this using mktemp
+  max_blocks = 1_000_000
+  file = Tempfile.new
+  begin
+    file.write(plaintext * max_blocks)
+
+    full_command = "#{command} -in #{file.path}"
+
+    puts "Bulk test: #{full_command}"
+    before = Time.now
+    result = IO.popen(full_command, mode: "r", encoding: Encoding::ASCII_8BIT) do |io|
+      io.read.unpack("H*")
+    end
+    after = Time.now
+    file.unlink
+    puts "Took #{after - before}s (bulk = #{plaintext.length * max_blocks})"
+    result
+  rescue Error => e
+    file.unlink
+    raise e
+  end
+end
+
+def compare(fake_openssl, real_openssl, plaintext)
+  fake_result = IO.popen(fake_openssl, mode: "r+") do |io|
+    io.write(plaintext)
+    io.close_write
+    io.read.unpack("H*")
+  end
+  real_result = IO.popen(real_openssl, mode: "r+") do |io|
+    io.write(plaintext)
+    io.close_write
+    io.read.unpack("H*")
+  end
+
+  raise "#{fake_openssl} produced #{fake_result} not #{real_result}" if fake_result != real_result
+
+  long_batch_plaintext = "1234567890abcdef1234567890abcdef1234567890abcdef"
+
+  fake_result = run_bulk(fake_openssl, long_batch_plaintext)
+  real_result = run_bulk(real_openssl, long_batch_plaintext)
+  raise "#{fake_openssl} produced a mismatching result (lengths #{fake_result.length}/#{real_result.length}" if fake_result != real_result
 end
 
 tests_done = 0
@@ -33,24 +76,7 @@ if defined? JdCrypt::Blowfish
   iv_hex = "00" * 8
   fake_openssl = "#{ENV["_"]} ./openssl_like.rb blowfish Blowfish #{key_hex} #{iv_hex}"
   real_openssl = "openssl enc -bf-cbc -K #{key_hex} -iv #{iv_hex}"
-  puts fake_openssl
-  before = Time.now
-  IO.popen(fake_openssl, mode: "r+") do |io|
-    io.write(plaintext)
-    io.close_write
-    puts io.read.unpack("H*")
-  end
-  after = Time.now
-  puts "Took #{after - before}s"
-  puts real_openssl
-  before = Time.now
-  IO.popen(real_openssl, mode: "r+") do |io|
-    io.write(plaintext)
-    io.close_write
-    puts io.read.unpack("H*")
-  end
-  after = Time.now
-  puts "Took #{after - before}s"
+  compare(fake_openssl, real_openssl, plaintext)
 end
 if defined? JdCrypt::AES
   tests_done += 1
@@ -58,24 +84,7 @@ if defined? JdCrypt::AES
   iv_hex = "00" * 16
   fake_openssl = "#{ENV["_"]} ./openssl_like.rb aes AES #{key_hex} #{iv_hex}"
   real_openssl = "openssl enc -aes-128-cbc -nosalt -K #{key_hex} -iv #{iv_hex}"
-  puts fake_openssl
-  before = Time.now
-  IO.popen(fake_openssl, mode: "r+") do |io|
-    io.write(plaintext)
-    io.close_write
-    puts io.read.unpack("H*")
-  end
-  after = Time.now
-  puts "Took #{after - before}s"
-  puts real_openssl
-  before = Time.now
-  IO.popen(real_openssl, mode: "r+") do |io|
-    io.write(plaintext)
-    io.close_write
-    puts io.read.unpack("H*")
-  end
-  after = Time.now
-  puts "Took #{after - before}s"
+  compare(fake_openssl, real_openssl, plaintext)
 end
 
 puts "No detectable block-encryption modules; skipping openssl tests" if tests_done.zero?
